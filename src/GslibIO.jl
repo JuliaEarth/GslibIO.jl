a46c90243d499c007bcf8982cf918efcd2a7867e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-12-22 15:23:42 -0800

    Add save method with flattened arrays

--- src/GslibIO.jl
+++ src/GslibIO.jl
@@ -4,13 +4,23 @@ module GslibIO
 
 using FileIO
 
-immutable GslibGRID{T<:AbstractFloat,A<:AbstractArray{T,3}}
+"""
+    GslibGRID
+
+Type holding all necessary information for saving/loading properties.
+"""
+struct GslibGRID{T<:AbstractFloat,A<:AbstractArray{T,3}}
   properties::Vector{A}
   propnames::Vector{String}
   origin::NTuple{3,T}
   spacing::NTuple{3,T}
 end
 
+"""
+    load(file)
+
+Load grid properties from `file`.
+"""
 function load(file::File{format"GSLIB"})
   f = open(file)
   fs = stream(f)
@@ -40,12 +50,15 @@ function load(file::File{format"GSLIB"})
   GslibGRID(properties, propnames, (ox,oy,oz), (dx,dy,dz))
 end
 
-function save(file::File{format"GSLIB"}, properties::Vector{A};
-              origin=(0.,0.,0.), spacing=(1.,1.,1.),
-              header="", propnames="") where {T<:AbstractFloat,A<:AbstractArray{T,3}}
-  # sanity checks
-  @assert length(Set(size.(properties))) == 1 "properties must have the same size"
+"""
+    save(file, properties, propsize; [optional parameters])
 
+Save 1D `properties`, which originally had 3D size `propsize`.
+"""
+function save(file::File{format"GSLIB"},
+              properties::Vector{V}, propsize::Tuple;
+              origin=(0.,0.,0.), spacing=(1.,1.,1.),
+              header="", propnames="") where {T<:AbstractFloat,V<:AbstractArray{T,1}}
   # default property names
   isempty(propnames) && (propnames = ["prop$i" for i=1:length(properties)])
   @assert length(propnames) == length(properties) "number of property names must match number of properties"
@@ -53,11 +66,8 @@ function save(file::File{format"GSLIB"}, properties::Vector{A};
   # convert vector of names to a long string
   propnames = join(propnames, " ")
 
-  # retrieve grid size
-  propsize = size(properties[1])
-
   # collect all properties in a big matrix
-  P = hcat([prop[:] for prop in properties]...)
+  P = hcat(properties...)
 
   open(file, "w") do f
     # write header
@@ -75,6 +85,30 @@ function save(file::File{format"GSLIB"}, properties::Vector{A};
   end
 end
 
+"""
+    save(file, properties, [optional parameters])
+
+Save 3D `properties` by first flattening them into 1D properties.
+"""
+function save(file::File{format"GSLIB"}, properties::Vector{A};
+              kwargs...) where {T<:AbstractFloat,A<:AbstractArray{T,3}}
+  # sanity checks
+  @assert length(Set(size.(properties))) == 1 "properties must have the same size"
+
+  # retrieve grid size
+  propsize = size(properties[1])
+
+  # flatten and proceed with pipeline
+  flatprops = [prop[:] for prop in properties]
+
+  save(file, flatprops, propsize, kwargs...)
+end
+
+"""
+    save(file, property)
+
+Save single 3D `property` by wrapping it into a singleton collection.
+"""
 function save(file::File{format"GSLIB"},
               property::A; kwargs...) where {T<:AbstractFloat,A<:AbstractArray{T,3}}
   save(file, [property]; kwargs...)
