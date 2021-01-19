536d7cdd6f5afcbedfb310b061fa8730c635b76f
Exequiel Manuel Sepulveda Escobedo <exequiel.sepulvedaescobedo@adelaide.edu.au>
2021-01-20 09:05:34 +1030

    This commit incorporante changes in unit test for load_legacy functionality.
    1.- The existing test for RegularGrid 'Legacy' is renamed to 'LegacyGrid'
    2.- A new test for PoinSet 'LegacyPointSet' is included that test a successful case
    3.- A new test for an invalid GSLIB file 'LegacyInvalid' is included

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,7 +39,7 @@ datadir = joinpath(@__DIR__,"data")
     rm(fname)
   end
 
-  @testset "Legacy" begin
+  @testset "LegacyGrid" begin
     fname = joinpath(datadir,"legacy_grid.gslib")
 
     sdata = GslibIO.load_legacy(fname, (2,2,2))
@@ -53,4 +53,31 @@ datadir = joinpath(@__DIR__,"data")
     @test isequal(lit, [1,2,3,4,5,6,7,8])
     @test isequal(sat, [0.8,0.7,0.8,0.7,0.8,0.7,0.8,NaN])
   end
+
+  @testset "LegacyPointSet" begin
+    fname = joinpath(datadir,"legacy_pset.gslib")
+
+    coordnames = (:East, :North, :Elevation)
+    sdata = GslibIO.load_legacy(fname, coordnames)
+    cdata = coordinates(sdata)
+    @test size(cdata) == (3, 4)
+    @test nelms(sdata) == 4
+    por = sdata[:Porosity]
+    @test isequal(por, [0.1, 0.2, 0.3, 0.4])
+    @test isequal(cdata[1, :], [10.0, 20.0, 30.0, 40.0])
+    @test isequal(cdata[2, :], [11.0, 21.0, 31.0, 41.0])
+    @test isequal(cdata[3, :], [12.0, 22.0, 32.0, 42.0])
+
+    # test when coordnames are not in varnames
+    coordnames = (:x, :y, :Elevation)
+    @test_throws AssertionError GslibIO.load_legacy(fname, coordnames)
+  end
+
+  @testset "LegacyInvalid" begin
+    fname = joinpath(datadir,"legacy_invalid.gslib")
+
+    coordnames = (:x, :y, :z)
+    @test_throws MethodError GslibIO.load_legacy(fname, coordnames)
+  end
+
 end
