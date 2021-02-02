b096e48a30af47a616c0341adcb927e9a3ccb592
exepulveda <exequiel.sepulvedaescobedo@adelaide.edu.au>
2021-02-02 10:57:17 +1030

    Implement load_legacy/save_legacy

--- src/GslibIO.jl
+++ src/GslibIO.jl
@@ -13,6 +13,14 @@ using GeoStatsBase
 # type aliases
 const Array2or3{T} = Union{AbstractArray{T,2},AbstractArray{T,3}}
 
+# Legacy specification of the GSLIB format: http://www.gslib.com/gslib_help/format.html
+# Note that in this context variables can be actual attributes as well as names of coordinates
+struct LegacySpec
+  header::String           # first line is a header in free format
+  varnames::Vector{Symbol} # variable names, one per line
+  data::Matrix{Float64}    # data matrix with variables as columns
+end
+
 """
     load(file)
 
@@ -44,6 +52,27 @@ function load(file::File{format"GSLIB"})
   end
 end
 
+# helper function to parse a legacy GSLIB file
+function parse_legacy(filename::AbstractString)
+  open(filename) do fs
+    # first line is a free header
+    header = readline(fs)
+
+    # the second line contains the number of variables (the first integer)
+    # and it may contain extra info (comments), which will be ignored
+    words = split(readline(fs))
+    nvars = parse(Int, words[1])
+
+    # variable names, one per line
+    varnames = [Symbol(strip(readline(fs))) for i in 1:nvars]
+
+    # read remaning content as data
+    data = readdlm(fs)
+
+    LegacySpec(header, varnames, data)
+  end
+end
+
 """
     load_legacy(filename, dims; origin=(0.,0.,0.), spacing=(1.,1.,1.), na=-999)
 
@@ -52,28 +81,43 @@ Optionally set the value used for missings `na`.
 """
 function load_legacy(filename::AbstractString, dims::NTuple{3,Int};
                      origin=(0.,0.,0.), spacing=(1.,1.,1.), na=-999)
-  open(filename) do fs
-    # skip header
-    readline(fs)
+  spec = parse_legacy(filename)                     
 
-    # number of properties
-    nvars = parse.(Int, readline(fs))
+  # handle missing values
+  replace!(spec.data, na=>NaN)
 
-    # properties names
-    vars = [Symbol(readline(fs)) for i in 1:nvars]
+  # create data dictionary
+  table = (; zip(spec.varnames, eachcol(spec.data))...)
+  domain = RegularGrid(dims, origin, spacing)
 
-    # read property values
-    X = readdlm(fs)
+  georef(table, domain)
+end
 
-    # handle missing values
-    replace!(X, na=>NaN)
+"""
+    load_legacy(filename, coordnames, na=-999)
 
-    # create data dictionary
-    data = (; zip(vars, eachcol(X))...)
-    domain = RegularGrid(dims, origin, spacing)
+Load legacy GSLIB `filename` into a PointSet using the properties in `coordnames` as coordinates.
+Optionally set the value used for missings `na`.
+"""
+function load_legacy(filename::AbstractString, coordnames=(:x, :y, :z); na=-999)
+  spec = parse_legacy(filename)
 
-    georef(data, domain)
-  end
+  @assert coordnames ⊆ spec.varnames && !isempty(coordnames) "invalid coordinate names"
+
+  # handle missing values
+  replace!(spec.data, na=>NaN)
+
+  # we need to identify and separate coordinates and actual attributes
+  coordinds = indexin(collect(coordnames), spec.varnames)
+  coords = spec.data[:,coordinds]'
+
+  # create table with varnames not in coordnames
+  attrinds = setdiff(1:length(spec.varnames), coordinds)
+  attrnames = spec.varnames[attrinds]
+  table = (; zip(attrnames, eachcol(spec.data[:,attrinds]))...)
+  domain = PointSet(coords)
+
+  georef(table, domain)
 end
 
 """
@@ -153,6 +197,52 @@ function save(file::File{format"GSLIB"}, sdata::AbstractData)
   save(file, collect(eachcol(table)), size(grid),
        origin=origin(grid), spacing=spacing(grid),
        propnames=vars)
+end
+
+# low level function for saving data to a legacy GSLIB format
+function save_legacy(filename::AbstractString, data::AbstractMatrix,
+                     varnames::NTuple, header::AbstractString, na)
+  @assert size(data, 2) == length(varnames) "invalid data for the specified variable names"
+  nvars = size(data, 2)
+
+  # handle missing values
+  datana = replace(data, NaN=>na)
+
+  open(filename, "w") do f
+    write(f, "$header\n")
+    write(f, "$nvars\n")
+    for v in varnames
+      write(f, "$v\n")
+    end
+    writedlm(f, datana, ' ')
+  end
+end
+
+"""
+    save_legacy(filename, sdata)
+
+Save spatial data `sdata` to `filename` in legacy GSLIB format. Optionally, specify
+the `coordnames`, the `header` and the value `na` used to represent missing entries.
+"""
+function save_legacy(filename::AbstractString, sdata::AbstractData; coordnames=(:x, :y, :z),
+                     header="# This file was generated with GslibIO.jl", na=-999.0)
+  table = values(sdata)
+  sdomain = domain(sdata)
+  
+  if sdomain isa PointSet
+    # add coordinates to data and coordnames to varnames
+    @assert ncoords(sdomain) == length(coordnames) "the length of coordinate names must be equal to the coordinate dimension"
+    varnames = [coordnames...; propertynames(table)]
+    data = [coordinates(sdomain)' Matrix(table)]
+  elseif sdomain isa RegularGrid
+    # a regular grid does not need to save coordinates
+    varnames = propertynames(table)
+    data = Matrix(table)
+  else
+    @error "can only save data defined on point sets or regular grids"
+  end
+
+  save_legacy(filename, data, Tuple(varnames), header, na)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -39,9 +39,31 @@ datadir = joinpath(@__DIR__,"data")
     rm(fname)
   end
 
-  @testset "Legacy" begin
-    fname = joinpath(datadir,"legacy.gslib")
+  @testset "LegacyParser" begin
+    fname = joinpath(datadir, "legacy_grid.gslib")
 
+    spec = GslibIO.parse_legacy(fname)
+
+    @test isequal(spec.data[:, 1], [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8])
+    @test isequal(spec.data[:, 2], [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
+    @test isequal(spec.data[:, 3], [0.8, 0.7, 0.8, 0.7, 0.8, 0.7, 0.8, -999.0])
+
+    @test spec.varnames == [:Porosity, :Lithology, Symbol("Water Saturation")]
+
+    fname = joinpath(datadir, "legacy_pset.gslib")
+
+    spec = GslibIO.parse_legacy(fname)
+
+    @test spec.varnames == [:East, :North, :Elevation, :Porosity]
+
+    @test isequal(spec.data[:, 1], [10.0, 20.0, 30.0, 40.0])
+    @test isequal(spec.data[:, 2], [11.0, 21.0, 31.0, 41.0])
+    @test isequal(spec.data[:, 3], [12.0, 22.0, 32.0, 42.0])
+    @test isequal(spec.data[:, 4], [0.1, 0.2, 0.3, 0.4])
+  end
+
+  @testset "LegacyGrid" begin
+    fname = joinpath(datadir,"legacy_grid.gslib")
     sdata = GslibIO.load_legacy(fname, (2,2,2))
     @test size(domain(sdata)) == (2,2,2)
     @test origin(domain(sdata)) == [0.,0.,0.]
@@ -52,5 +74,44 @@ datadir = joinpath(@__DIR__,"data")
     @test isequal(por, [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8])
     @test isequal(lit, [1,2,3,4,5,6,7,8])
     @test isequal(sat, [0.8,0.7,0.8,0.7,0.8,0.7,0.8,NaN])
+
+    # test if storing/loading recovers data
+    fname = tempname()*".gslib"
+    GslibIO.save_legacy(fname, sdata, na=-999)
+    ndata = GslibIO.load_legacy(fname, (2,2,2), na=-999)
+    @test isequal(domain(sdata), domain(ndata))
+    @test isequal(values(sdata), values(ndata))
+    rm(fname)
+  end
+
+  @testset "LegacyPointSet" begin
+    fname = joinpath(datadir,"legacy_pset.gslib")
+    coordnames = (:East, :North, :Elevation)
+    sdata = GslibIO.load_legacy(fname, coordnames)
+    cdata = coordinates(sdata)
+    @test size(cdata) == (3, 4)
+    @test nelms(sdata) == 4
+    por = sdata[:Porosity]
+    @test isequal(por, [0.1, 0.2, 0.3, 0.4])
+    @test isequal(cdata[1, :], [10.0, 20.0, 30.0, 40.0])
+    @test isequal(cdata[2, :], [11.0, 21.0, 31.0, 41.0])
+    @test isequal(cdata[3, :], [12.0, 22.0, 32.0, 42.0])
+
+    # test when coordnames are not in varnames
+    coordnames = (:x, :y, :Elevation)
+    @test_throws AssertionError GslibIO.load_legacy(fname, coordnames)
+
+    # test if storing/loading recovers data
+    fname = tempname()*".gslib"
+    GslibIO.save_legacy(fname, sdata, coordnames=coordnames)
+    ndata = GslibIO.load_legacy(fname, coordnames)
+    @test sdata == ndata
+    rm(fname)
+  end
+
+  @testset "LegacyInvalid" begin
+    fname = joinpath(datadir,"legacy_invalid.gslib")
+    coordnames = (:x, :y, :z)
+    @test_throws MethodError GslibIO.load_legacy(fname, coordnames)
   end
 end
